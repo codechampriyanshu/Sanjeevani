b0d03bc6951ea4c99cb11bdf217e4b460d5423dd
Ashish Verma <ashishkv.2016@gmail.com>
2021-12-06 17:04:27 +0530

    set up router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+
+      </Routes>
+    </Router>
   );
 }
 
