c305ec8177dd43b5482f84e1ff8119fc9e1db826
Priyanshu Raj <priyanshuraj6649@gmail.com>
2021-12-07 14:15:51 +0530

    testimonials done

--- frontend/src/Components/Testimonials/Testimonials.jsx
+++ frontend/src/Components/Testimonials/Testimonials.jsx
@@ -22,29 +22,6 @@ const config = {
   };
 
 export default function Testimonials() {
-   const [settings,setSettings]=useState(config)
-   useEffect(()=>{
-     const handleResize=()=>{
-       const size=window.innerWidth
-       if(size<300){
-         setSettings({...settings,slidesToShow:1,centerMode:false})
-       }
-       else if(size<=480){
-         setSettings({...settings,slidesToShow:1,centerMode:true})
-       }else if(size<=800){
-        setSettings({...settings,slidesToShow:2,centerMode:true})
-       }else if(size<=1200){
-        setSettings({...settings,slidesToShow:3,centerMode:true})
-       }else{
-        setSettings({...settings,slidesToShow:4,centerMode:true})
-       }
-     }
-     window.addEventListener("resize",handleResize)
-     handleResize()
-     return ()=>{
-       window.removeEventListener("resize",handleResize)
-     }
-   },[])
 
     const sliderRef=useRef()
     const goNext=()=>{
@@ -54,17 +31,17 @@ export default function Testimonials() {
         sliderRef.current.slickPrev()
     }
     return (
-        <div>
-        <span className="block pt-10 text-center text-blue-800 md:text-3xl md:font-bold md:mt-6">What people have to say....</span>
-        <div className="relative w-2/5 mx-auto">
+        <div className="md:mb-8">
+        <span className="block pt-10 text-center text-blue-800 md:text-3xl md:font-bold md:mt-6">What people say....</span>
+        <div className="relative w-full mx-auto md:w-2/3 xl:w-1/2">
         <button onClick={goPrev}><img src="/images/left-arrow.png" alt="" className="absolute z-20 w-6 h-6 rounded-full md:h-8 md:w-8 opacity-30 left-2 top-1/3"/></button>
-            <Slider ref={sliderRef} {...settings} className="w-full m-0">
+            <Slider ref={sliderRef} {...config} className="w-full m-0">
                 {images.map((x, i) => {
-                    return <div key={i} className="h-60 img-card bnine:h-80">
-                    <img className="object-cover w-1/3 mx-auto mt-3 rounded-full h-36 ring-white ring" src={x.img} alt=""/>
+                    return <div key={i} className="h-48 xs:h-60 sm:h-64 md:h-72 img-card bnine:h-80">
+                    <img className="object-cover w-1/4 mx-auto mt-3 mb-2 rounded-full lg:mb-7 sm:mb-4 h-1/3 lg:w-1/5 md:h-2/5 md:w-1/4 sm:w-1/5 xs:w-1/4 xs:h-2/5 ring-white ring" src={x.img} alt=""/>
                     <div className="card-body">
-                        <div className="mb-2 text-base font-semibold md:text-lg lg:text-xl">{x.title}</div>
-                        <div className="hidden text-sm sm:block bnine:mt-3 md:text-base">{x.text}</div>
+                        <div className="mb-2 text-sm font-semibold xs:text-base sm:text-lg md:text-xl xl:text-xl">{x.title}</div>
+                        <div className="text-xs xs:text-sm bnine:mt-3 sm:text-base md:text-lg">{x.text}</div>
                     </div>
                     </div>
                 })}
