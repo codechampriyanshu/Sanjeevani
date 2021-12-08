63a0968a2059afba2f4bb855056907711f358c9f
Priyanshu Raj <priyanshuraj6649@gmail.com>
2021-12-08 19:21:49 +0530

    navbar commited

--- frontend/src/Components/Navbar/Nav.jsx
+++ frontend/src/Components/Navbar/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav({toggle,setToggle}) {
     setToggle(!toggle)
   }
   return (
-    <div>
+    <div >
       <div className="md:hidden">
         <img src="/images/logo.jpg" className="absolute z-50 w-12 h-12 top-3 left-3" alt="logo"/>
         <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" className={(toggle?"text-white ":"text-black ")+ "absolute z-50 w-8 h-8 top-3 right-3"} viewBox="0 0 20 20" fill="currentColor">
@@ -22,15 +22,20 @@ export default function Nav({toggle,setToggle}) {
         <Link className="pt-4 pl-4 text-lg font-bold" to="/help">Help</Link>
         <Link className="pt-4 pl-4 text-lg font-bold" to="/about">About</Link>
       </div>
-      <div className="hidden md:block">
-        <img src="/images/logo.jpg" className="absolute z-50 w-12 h-12 top-3 left-3" alt="logo"/>
-        <Link className="pt-8 pl-4 text-lg font-bold" to="/">Home</Link>
-        <Link className="pt-8 pl-4 text-lg font-bold" to="/profile">My profile</Link>
-        <Link className="pt-4 pl-4 text-lg font-bold" to="/appointments">Appointments</Link>
-        <Link className="pt-4 pl-4 text-lg font-bold" to="/consultations">Consultations</Link>
-        <Link className="pt-4 pl-4 text-lg font-bold" to="/records">My Records</Link>
-        <Link className="pt-4 pl-4 text-lg font-bold" to="/help">Help</Link>
-        <Link className="pt-4 pl-4 text-lg font-bold" to="/about">About</Link>
+      <div className="items-center justify-between hidden lg:px-4 md:flex">
+        <div >
+          <img src="/images/logo.jpg" className="z-50 w-14 h-14" alt="logo"/>
+        </div>
+        <div className="px-2 py-2 mt-3 ">
+        <Link className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/">Home</Link>
+        <Link className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/profile">My profile</Link>
+        <Link  className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/help">Help</Link>
+        <Link  className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/about">About</Link>
+        <Link  className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/login">Login</Link>
+        <Link  className="px-3 py-1 mr-4 text-base font-bold border-2 rounded-lg lg:text-lg hover:bg-black hover:text-white hover:text-center" to="/signup">Sign In</Link>
+
+        
+        </div>
       </div>
     </div>
   )
