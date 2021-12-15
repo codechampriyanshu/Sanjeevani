885449fced8e70fa582160bd5abfdb81a5ae18d0
Priyanshu Raj <priyanshuraj6649@gmail.com>
2021-12-15 07:53:04 +0530

    commited before mid-term evaluation

--- frontend/src/Components/Patient/MediHistory.jsx
+++ frontend/src/Components/Patient/MediHistory.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const MediHistory = () => {
-    {/* fetch data of all the activities of the patient in an array */}
-    const arr=[
-       {
-            doctorName:"Asish",
-            date:"12 jan 2000",
-            issues:"",
-            medicines:["med1","med2","med3"],
-            tests:{
-                test1:"",
-                test2:"",
-            }
-        },
-
-        {
-            
-        }
-    ]
+    const [val,setval]=useState({
+        Patient:"Priyanshu Raj",
+        disease:"tuberclosis",
+        consultanthospital:"CMC vellore",
+        consultantdoctor:"Dr Sony",
+        drrepo:"patient has  severe tb since 5 years and is now in critical condition",
+        medicine:"mogid-md500 Nicardia 5 "
+    })
     return (
-        <div>
-            <div className=""></div>
-            
+        <div className='flex flex-col items-center w-full font-serif font-bold'>
+            <span className='p-4 md:pt-7'>{val.Patient}</span>
+            <span className='p-4 md:pt-7'>{val.disease}</span>
+            <span className='p-4 md:pt-7'>{val.consultanthospital}</span>
+            <span className='p-4 md:pt-7'>{val.consultantdoctor}</span>
+            <span className='p-4 md:pt-7'>{val.drrepo}</span> 
+            <span className='p-4 md:pt-7'>{val.medicine}</span>
+        
         </div>
     )
 }
