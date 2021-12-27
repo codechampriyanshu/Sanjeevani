32c94f2ee0ca860b98e514bec53b9c3e109dd2d1
Ashish Verma <ashishkv.2016@gmail.com>
2021-12-27 21:01:36 +0530

    Revert "finishing up"
    This reverts commit d891d7fcbefe8a7af8adb4131dc84b4467ddb5a6.

--- backend/models/mediHistory.js
+++ backend/models/mediHistory.js
@@ -2,13 +2,13 @@ const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 
 const historySchema= new Schema({           //TODO: upload reports/prescriptions too
-    what:{type:String,required:true},       //disease or test name
-    fromWhen:String,                        //when is he suffering from / when the test is done
-    treated:{type:Boolean,required:true},    //whether patient is cured or still suffering
-    institute:{type:String,required:true},     //hospital/clinic/pathology lab
-    appointedDate:{type:String,default:""},    //when the test is done
-    comments:{type:String},                     //any notes by patient
-    patient_id:{type:String,required:true}        //to map history with corresponding patient
+    what:{type:String,required:true},
+    fromWhen:String,
+    treated:{type:Boolean,required:true},
+    institute:{type:String,required:true},
+    appointedDate:{type:String,default:""},
+    comments:{type:String},
+    patient_id:{type:String,required:true}
 })
 
 const History=mongoose.model('history',historySchema)
