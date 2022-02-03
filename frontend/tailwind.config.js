902852347850a5fff0d04f470b60ac3375551777
Ashish Verma <ashishkv.2016@gmail.com>
2022-02-03 16:46:49 +0530

    upgraded tailwind and react-scripts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,15 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    screens:{
-        'xs': '435px',
-        'between' : '512px',
-        'sm' : '640px',
-        'md' : '768px',
-        'bnine': '900px',
-        'lg' : '1024px',
-        'xl' : '1280px',
-        '2xl' : '1536px',
-    },
     extend: {
-        // backgroundImage: {
-        //     'hero-pattern':`url(${process.env.PUBLIC_URL}/images/bubbles.svg)`
-        // },
       colors: {
           
       },
       fontFamily: {
-          sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
+          
       },
       //font customization help: https://www.youtube.com/watch?v=sOnBG2wUm1s
       },
-  },
-  variants: {
-    extend: {
-      display: ["group-hover"],
-    },
   },
   plugins: [],
 }
